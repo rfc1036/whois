6520b68285fc0aa65829cb4b146908d99af0bef8
Marco d'Itri <md@linux.it>
2021-02-16 01:53:41 +0100

    Update the copyright year

--- whois.c
+++ whois.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1999-2019 Marco d'Itri <md@linux.it>.
+ * Copyright (C) 1999-2021 Marco d'Itri <md@linux.it>.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
