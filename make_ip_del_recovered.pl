113b4cb06887ac1b22173cb35bc66a191d8edd07
Marco d'Itri <md@linux.it>
2023-05-13 20:14:42 +0200

    make_ip_del_recovered.pl: skip redundant entries
    Makes it ignore the entries for recovered networks which are already
    covered by entries in ip_del_list.

--- make_ip_del_recovered.pl
+++ make_ip_del_recovered.pl
@@ -6,10 +6,12 @@ use strict;
 use autodie;
 
 use Text::CSV;
+use Net::Patricia;
 use Net::CIDR;
 use Net::IP;
 
 my $csv = Text::CSV->new;
+my $pt = parse_ip_del('ip_del_list');
 
 open(my $in, '<', 'ipv4-recovered-address-space-2.csv');
 open(my $out, '>', 'ip_del_recovered.h');
@@ -19,10 +21,16 @@ while (my $row = $csv->getline($in)) {
 	next if $row->[5] ne 'ALLOCATED';
 	my ($first_ip, $last_ip, undef, undef, $server) = @$row;
 
-	print $out "/* $first_ip - $last_ip */\n";
 	my @networks =
+		grep {
+			my $server_recovered = $pt->match_string($_->ip);
+			$server_recovered and $server_recovered ne $server;
+		}
 		map { Net::IP->new($_) }
 		Net::CIDR::range2cidr($first_ip . '-' . $last_ip);
+	next if not @networks;
+
+	print $out "/* $first_ip - $last_ip */\n";
 	print $out sprintf(qq|{ %sUL, %sUL, "%s" },\n|,
 		$_->intip,
 		((~(0xffffffff >> $_->prefixlen)) & 0xffffffff),
@@ -32,4 +40,30 @@ while (my $row = $csv->getline($in)) {
 
 close($in);
 close($out);
+exit;
+
+sub parse_ip_del {
+	my ($file) = @_;
+
+	my $pt = new Net::Patricia;
+
+	open(my $in, '<', $file);
+	while (<$in>) {
+		# this code is copied from make_ip_del.pl
+		chomp;
+		s/#.*$//;
+		s/^\s+//; s/\s+$//;
+		next if /^$/;
+
+		die "format error: $_" if not /^([\d\.]+)\/(\d+)\s+([\w\.]+)$/;
+		my $network = "$1/$2";
+		my $server = $3;
+
+		$server = "whois.$server.net" if $server !~ /\./;
+
+		$pt->add_string($network, $server) or die;
+	}
+
+	return $pt;
+}
 
